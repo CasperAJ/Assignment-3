69b5fd9bd3255d2021ca43449eece30939d74dba
Casper <CasperAJ@users.noreply.github.com>
2018-10-12 18:16:22 +0200

    All code documented
    The code has new been documented with comments explaning their purpose.
    Some unncessesary features has been removed for cleanup

--- Server/Program.cs
+++ Server/Program.cs
@@ -46,30 +46,29 @@ namespace Server
 
         public static void HandleClient(object client)
         {
-
+            // Tells new connection has been received and a new thread has been created to take care of it, post it in cw along with thread id
             Console.WriteLine("New thread created");
             Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
-            Thread.Sleep(10000);
+            // Initiate a instance of the RawData Json Transport Protocol (RWJTP) used for converting to/from JSON
+            RawDataJTP RWJTP = new RawDataJTP();
+
+            // Casts the connection back to a tcpClient and gets the clients stream
             TcpClient newClient = (TcpClient)client;
             var stream = newClient.GetStream();
 
-            //TcpClient Nclient = (TcpClient)client;
-
-            //sets two streams
-            //StreamWriter sWriter = new StreamWriter(Nclient.GetStream(), Encoding.ASCII);
-            //StreamReader sReader = new StreamReader(Nclient.GetStream(), Encoding.ASCII);
-
-            //Console.WriteLine("client" + sReader.ReadLine());
-            //var sReader = new StreamReader(newClient.GetStream());
-
+            // Creates the buffer for the stream
             var buffer = new byte[newClient.ReceiveBufferSize];
 
+            // Reads the clients msg from the stream, and encodes it back to a string in JSON format
             var readClient = stream.Read(buffer, 0, buffer.Length);
             var utf8ClientMsg = Encoding.UTF8.GetString(buffer, 0, readClient);
-            RawDataJTP RWJTP = new RawDataJTP();
+
+            // Hands over the request to WRJTP for deserialization, and returns a Request Object
             var clientRequest = RWJTP.RWJTP_Request(utf8ClientMsg);
+            // Hands over the Request Object to the WRJTP again in order to perform logic and return a Response Object that can be returned to the requester
             var responseToClient = RWJTP.RWJTP_Response(clientRequest);
 
+            // Writes the response back to the requester
             stream.WriteAsync(buffer, 0 ,responseToClient.Length);
 
             Console.WriteLine(responseToClient);
